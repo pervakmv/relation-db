bf2e9dc9e7938bea15612d67d4eff2e7de577038
Nikolay <pervakmv@elas.kiev.ua>
2018-10-01 11:31:30 +0300

    Второй урок по БД

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        //java
+        //1. create class with fields +
+        //2. create object with some values
+        //3. using it
+
+        //db
+        //1. create table with colums -
+        //2. insert some date to the table
+        //3. useing it
+
+    }
+}
---
+++ Order.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+
+public class Order {
+    private long id;
+    private String productName;
+    private int  price;
+    private Date deateOrdered;
+    private Date dateConfirmed;
+
+
+    public Order(long id, String productName, int price, Date deateOrdered, Date dateConfirmed) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.deateOrdered = deateOrdered;
+        this.dateConfirmed = dateConfirmed;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Date getDeateOrdered() {
+        return deateOrdered;
+    }
+
+    public Date getDateConfirmed() {
+        return dateConfirmed;
+    }
+}
